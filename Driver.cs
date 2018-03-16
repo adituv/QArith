6b2bcfbd275c5b766ee5284135044332619513f6
Iris Ward <aditu.venyhandottir@gmail.com>
2018-03-16 01:59:16 +0000

    Sample quantum analog to addition

---
+++ Driver.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+using System;
+using System.Numerics;
+
+namespace QArith
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                var thetas1 = new double[4];
+                var phis1 = new double[4];
+                var thetas2 = new double[4];
+                var phis2 = new double[4];
+                var randGen = new Random();
+
+                for(int i = 0; i < 4; i++) {
+                    thetas1[i] = randGen.NextDouble() * Math.PI;
+                    phis1[i] = randGen.NextDouble() * 2 * Math.PI;
+                    thetas2[i] = randGen.NextDouble() * Math.PI;
+                    phis2[i] = randGen.NextDouble() * 2 * Math.PI;
+                }
+
+                var res = AddTest.Run(sim, new QArray<double>(thetas1), new QArray<double>(phis1), new QArray<double>(thetas2), new QArray<double>(phis2)).Result;
+                var (sum,op2) = res;
+                var op1 = (sum - op2 + 16) % 16;
+
+                System.Console.WriteLine("QInt #1:");
+                for(int i = 0; i < 4; i++) {
+                    var (x,y) = FromBlochSphere(thetas1[i], phis1[i]);
+                    System.Console.Write(
+                        $"({x:N2}|0> + ({y.Real:N2} + {y.Imaginary:N2}i|1>) "
+                    );
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine("Bit probabilities:");
+                for(int i = 0; i < 4; i++) {
+                    var (x,y) = FromBlochSphere(thetas1[i], phis1[i]);
+                    System.Console.Write(
+                        $"{y.Magnitude:N2} "
+                    );
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+                System.Console.WriteLine("QInt #2:");
+                for(int i = 0; i < 4; i++) {
+                    var (x,y) = FromBlochSphere(thetas2[i], phis2[i]);
+                    System.Console.Write(
+                        $"({x:N2}|0> + ({y.Real:N2} + {y.Imaginary:N2}i|1>) "
+                    );
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine("Bit probabilities:");
+                for(int i = 0; i < 4; i++) {
+                    var (x,y) = FromBlochSphere(thetas2[i], phis2[i]);
+                    System.Console.Write(
+                        $"{y.Magnitude:N2} "
+                    );
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+                System.Console.WriteLine(
+                    $"Measured sum: {sum,3}.  Measured op2: {op2,3}.  Inferred op1: {op1,3}."
+                );
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+
+        static (Double, Complex) FromBlochSphere(double theta, double phi) {
+            var xcomp = Math.Cos(theta/2);
+            var ycomp = Complex.Exp(phi * Complex.ImaginaryOne) * Math.Sin(theta/2);
+
+            return (xcomp, ycomp);
+        }
+    }
+}
---
+++ Operation.qs
@@ -0,0 +1,67 @@
+﻿namespace QArith
+{
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Primitive;
+
+    // "Adds" op2 to op1
+    operation QAdd (op1 : LittleEndian, op2 : LittleEndian) : ()
+    {
+        body
+        {
+            using(ancillae = Qubit[2]) {
+                let carry = ancillae[0];
+                let temp = ancillae[1];
+
+                for(i in 0..4) {
+                    Reset(temp);
+
+                    SWAP(carry, temp);
+                    CNOT(op1[i], temp);
+                    CNOT(op2[i], temp);
+                    (Controlled X)([op1[i]; op2[i]], carry);
+                    SWAP(temp, op1[i]);
+                }
+
+                Reset(carry);
+                Reset(temp);
+            }
+        }
+    }
+
+    operation QubitFromRotations(theta : Double, phi : Double, qubit : Qubit) : ()
+    {
+        body {
+            Reset(qubit);
+
+            R(PauliX, theta, qubit);
+            R(PauliZ, phi, qubit);
+        }
+    }
+
+    operation AddTest(thetas1 : Double[], phis1 : Double[], thetas2 : Double[], phis2 : Double[]) : (Int,Int)
+    {
+        body {
+            mutable result = (0,0);
+
+            using(op1 = Qubit[5]) {
+            using(op2 = Qubit[5]) {
+                for(i in 0..3) {
+                    QubitFromRotations(thetas1[i], phis1[i], op1[i]);
+                    QubitFromRotations(thetas2[i], phis2[i], op2[i]);
+                }
+
+                let addOp1 = LittleEndian(op1);
+                let addOp2 = LittleEndian(op2);
+                QAdd(addOp1, addOp2);
+
+                set result = (MeasureInteger(addOp1), MeasureInteger(addOp2));
+
+                ResetAll(op1);
+                ResetAll(op2);
+            }
+            }
+
+            return result;
+        }
+    }
+}
